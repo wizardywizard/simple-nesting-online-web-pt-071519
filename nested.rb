835a80e3fdefd4cade149850d4eadfdac7ef9826
David Little <littledavid175@gmail.com>
2019-07-20 21:49:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,9 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
+return programmer_hash[:alan_kay][:known_for]}
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
