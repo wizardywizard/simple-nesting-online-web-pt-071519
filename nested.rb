52bb8e2dffe0ec03f2397287edac9c0b71c2038e
David Little <littledavid175@gmail.com>
2019-07-20 21:53:11 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages] = 'C'
 end
 
 def adding_matz
@@ -66,6 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		 :yukihiro_matsumoto => {
+ 		  :known_for => "Ruby",
+ 		  :languages => ["LISP", "C"]
+ 		},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -101,7 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
      
 end
 
@@ -121,5 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+ return programmer_hash
 end
